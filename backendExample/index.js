d61c710b049feb7285d57825d147c8720b1a999f
garthdp <garthdp4@gmail.com>
2024-08-04 13:26:57 +0200

    started backend project

---
+++ backendExample/index.js
@@ -0,0 +1,28 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import 'dotenv/config'
+//import ReviewDAO from "./dao/reviewsDAO.js"
+
+const MongoClient = mongodb.MongoClient
+const mongo_username = process.env['MONGO_USERNAME']
+const mongo_password = process.env['MONGO_PASSWORD']
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@firstcluster.0or611d.mongodb.net/?retryWrites=true&w=majority&appName=FirstCluster`
+
+const port = 8000
+
+MongoClient.connect(
+    uri,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500
+    }
+)
+.catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+})
+.then(async client => {
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`)
+    })
+})
